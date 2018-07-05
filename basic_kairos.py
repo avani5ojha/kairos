eb80c5be90253a703ad53581010f96c035fbae81
avani5ojha <39522571+avani5ojha@users.noreply.github.com>
2018-07-05 12:44:20 +0530

    Update basic_kairos.py

--- basic_kairos.py
+++ basic_kairos.py
@@ -1,10 +1,9 @@
 import kairos_face as kf
 
-# kf.settings.app_id='7fa6bda7'
-# kf.settings.app_key='0a782b95e6fb05aa932e55f4bcc47453'
 
-kf.settings.app_id='7fa6bda7'
-kf.settings.app_key='0a782b95e6fb05aa932e55f4bcc47453'
+
+kf.settings.app_id='ypur app id'
+kf.settings.app_key='your app key'
 
 # galleries_list=kf.get_galleries_names_list()
 #
