9388b9aff26383c317a7d2836da4288c05269345
avani5ojha <39522571+avani5ojha@users.noreply.github.com>
2018-07-08 19:12:59 +0530

    enroll new student

---
+++ enroll.py
@@ -0,0 +1,18 @@
+import kairos_face as kf
+import pymysql
+
+kf.settings.app_id=""
+kf.settings.app_key=""
+
+db=pymysql.connect("localhost", "root", "avani@mysql", "test")
+cursor=db.cursor()
+
+# sql2="select sid from test1 order by sid desc limit 1;"
+sql2="SELECT MAX(sid) FROM test1;"
+cursor.execute(sql2)
+result=cursor.fetchone()
+
+
+db.close()
+
+kf.enroll_face(file='face.jpg', subject_id=result[0] , gallery_name="student")
